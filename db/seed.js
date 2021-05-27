9240c163f23bd8252b35188d2ce36e0e6205bf94
Marcus Andreas Aandahl <marcus.aandahl@gmail.com>
2021-05-27 11:08:53 +0200

    Added seeding

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,9 @@
-//WRITE SEEDS HERE
+var db = require('./db');
+//Import Models used in seeding here:
+
+function seed(){
+    //WRITE SEEDS HERE
+    process.exit();
+}
+
+module.exports = { seed };
